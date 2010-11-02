afa39866b38d76c9514614edf4512c062c950f76
ahmed.badran@gmail.com <ahmed.badran@gmail.com@6ad2763e-12d5-d107-9526-56a298e86ecc>
2010-11-02 06:16:16 +0000

    Changed the resize mode for the buttons

--- Meeting Organizer/MainForm.Designer.cs
+++ Meeting Organizer/MainForm.Designer.cs
@@ -207,9 +207,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 19);
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(3, 16);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(145, 23);
+            this.button1.Size = new System.Drawing.Size(188, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Schedule Meeting";
             this.button1.UseVisualStyleBackColor = true;
@@ -217,9 +219,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(23, 48);
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(3, 39);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.Size = new System.Drawing.Size(188, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "View Upcoming meetings";
             this.button2.UseVisualStyleBackColor = true;
