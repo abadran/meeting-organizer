3d626bd4cfe8e5bc022bab536cbc94106c7a4697
ahmed.badran@gmail.com <ahmed.badran@gmail.com@6ad2763e-12d5-d107-9526-56a298e86ecc>
2010-11-03 06:35:54 +0000

    Completed adding the login dialog.

---
+++ Meeting Organizer/LoginDialog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Meeting_Organizer
+{
+    public partial class LoginDialog : Form
+    {
+        public LoginDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void LoginDialog_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
