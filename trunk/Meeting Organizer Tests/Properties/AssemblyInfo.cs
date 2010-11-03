442d455ce2a22bedcb6e9498d21b3b11cfff00c2
ahmed.badran@gmail.com <ahmed.badran@gmail.com@6ad2763e-12d5-d107-9526-56a298e86ecc>
2010-11-03 05:38:02 +0000

    Updates after adding unit tests.

--- trunk/Meeting Organizer Tests/Properties/AssemblyInfo.cs
+++ trunk/Meeting Organizer Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Meeting Organizer")]
+[assembly: AssemblyTitle("Meeting Organizer Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("The Amazing Company")]
-[assembly: AssemblyProduct("Meeting Organizer")]
-[assembly: AssemblyCopyright("Copyright © The Amazing Company 2010")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Meeting Organizer Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b582a902-91f3-4dfe-bf3c-ddd19d22db26")]
+[assembly: Guid("50664bc9-73e8-40f2-9b6b-efb9773b770e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,8 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Meeting Organizer Tests")]
