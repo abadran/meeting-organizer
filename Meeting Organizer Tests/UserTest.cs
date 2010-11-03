f29937bbde4d087e590fd6ea82fb01bc8e7e21c3
ahmed.badran@gmail.com <ahmed.badran@gmail.com@6ad2763e-12d5-d107-9526-56a298e86ecc>
2010-11-03 05:38:02 +0000

    Updates after adding unit tests.

---
+++ Meeting Organizer Tests/UserTest.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Meeting_Organizer;
+
+namespace Meeting_Organizer_Tests
+{
+    /// <summary>
+    /// Summary description for UserTest
+    /// </summary>
+    [TestClass]
+    public class UserTest
+    {
+        public UserTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        /// <summary>
+        ///A test for User Constructor
+        ///</summary>
+        [TestMethod()]
+        public void UserConstructorTest()
+        {
+            string n = string.Empty; // TODO: Initialize to an appropriate value
+            string l = string.Empty; // TODO: Initialize to an appropriate value
+            User target = new User(n, l);
+            Assert.AreEqual(target.Login, l);
+            Assert.AreEqual(target.Name, n);
+        }
+
+        /// <summary>
+        ///A test for User Constructor
+        ///</summary>
+        [TestMethod()]
+        public void UserConstructorTest1()
+        {
+            User target = new User();
+            Assert.AreEqual(target.Login, "");
+            Assert.AreEqual(target.Name, "");
+        }
+
+        /// <summary>
+        ///A test for Login
+        ///</summary>
+        [TestMethod()]
+        public void LoginTest()
+        {
+            User target = new User(); // TODO: Initialize to an appropriate value
+            string expected = string.Empty; // TODO: Initialize to an appropriate value
+            string actual;
+            target.Login = expected;
+            actual = target.Login;
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Name
+        ///</summary>
+        [TestMethod()]
+        public void NameTest()
+        {
+            User target = new User(); // TODO: Initialize to an appropriate value
+            string expected = string.Empty; // TODO: Initialize to an appropriate value
+            string actual;
+            target.Name = expected;
+            actual = target.Name;
+            Assert.AreEqual(expected, actual);
+//            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        [TestMethod()]
+        public void LoginAndNameTest()
+        {
+            User target = new User("my name", "mylogin");
+            Assert.AreEqual(target.Login, "mylogin");
+            Assert.AreEqual(target.Name, "my name");
+        }
+    }
+}
