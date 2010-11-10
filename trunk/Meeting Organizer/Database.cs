c61805d0721a632d2343f8228ea21a39016892bc
ahmed.badran@gmail.com <ahmed.badran@gmail.com@6ad2763e-12d5-d107-9526-56a298e86ecc>
2010-11-10 11:01:42 +0000

    A whole bunch of updates to read and write from the DB.

--- trunk/Meeting Organizer/Database.cs
+++ trunk/Meeting Organizer/Database.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Windows.Forms;
 
 namespace Meeting_Organizer
 {
-    class Database
+    public class Database
     {
-        private MeetingOrganizer organizerDB;
+        private MeetingOrganizer db;
         public Database()
         {
             string filePath = Properties.Settings.Default.DBConnection;
@@ -16,13 +17,28 @@ namespace Meeting_Organizer
             {
                 filePath = "Data Source=C:\\Users\\Ahmed\\Documents\\Visual Studio 2010\\Projects\\Meeting Organizer\\Meeting Organizer\\Meeting Organizer.sdf";
             }
-            organizerDB = new MeetingOrganizer(filePath);
+            db = new MeetingOrganizer(filePath);
         }
 
+        public User getUserWithLogin(string login)
+        {
+            IEnumerable<User> users = from p in db.User
+                        where (p.Login == login)
+                        select p;
+            if (users.Count() != 0)
+            {
+                return users.ElementAt(0);
+            }
+            else
+            {
+                return null;
+            }
+        }
+        // get the user with a particular login and password.
         public User getUserWithLoginAndPassword(string login, string password)
         {
 
-            IEnumerable<User> users = from p in organizerDB.User
+            IEnumerable<User> users = from p in db.User
                         where (p.Login == login) && (p.Password == password)
                         select p;
             if (users.Count() != 0)
@@ -35,15 +51,77 @@ namespace Meeting_Organizer
             }
         }
 
-        public void CreateNewUser(User user)
+        // create a new user in the db.
+        public void createNewUser(User user)
         {
-            organizerDB.User.InsertOnSubmit(user);
-            organizerDB.SubmitChanges();
+            db.User.InsertOnSubmit(user);
+            db.SubmitChanges();
         }
 
-        public DailySchedule getScheduleForUser(string login)
+        // get the list of notifications for a user.
+        public Notifications getNotificationsForUser(User user)
         {
-            return null;
+            IEnumerable<Event>  eventList = from usr in db.User
+                              join evtInvitee in db.EventInviteeRelation on usr.Id equals evtInvitee.InviteeId 
+                              join evt in db.Event on evtInvitee.EventId equals evt.Id
+                              where (evtInvitee.InviteeResponse == 0) && (evtInvitee.InviteeId == user.Id) 
+                              select evt;
+            return new Notifications(eventList);
+        }
+
+        // get list of users.
+        public User[] getUsers()
+        {
+            IEnumerable<User> users = from p in db.User
+                                        select p;
+            return users.ToArray<User>();
+        }
+
+        // get list of users except for the one with login loginOfUserToSkip.
+        public User[] getUserNamesSkipping(string loginOfUserToSkip)
+        {
+            IEnumerable<User> users = from p in db.User
+                                        where p.Login != loginOfUserToSkip
+                                        select p;
+            return users.ToArray<User>();
+        }
+
+        public User getUserWithId(int id)
+        {
+            IEnumerable<User> users = from p in db.User
+                        where (p.Id == id)
+                        select p;
+            if (users.Count() != 0)
+            {
+                return users.ElementAt(0);
+            }
+            else
+            {
+                return null;
+            }
+        }        
+
+        public void createInvitation(Invitation invitation)
+        {
+            db.Event.InsertOnSubmit(invitation.evt);
+            db.SubmitChanges();
+            //MessageBox.Show(invitation.evt.Id.ToString(), "foo", MessageBoxButtons.OK);
+            foreach (User user in invitation.users) {
+                EventInviteeRelation ei = new EventInviteeRelation();
+                ei.EventId = invitation.evt.Id;
+                ei.InviteeId = user.Id;
+                db.EventInviteeRelation.InsertOnSubmit(ei);
+            }
+            db.SubmitChanges();
+        }
+
+        public User[] getInviteesForEvent(Event e)
+        {
+            IEnumerable<User>  users = from usr in db.User
+                              join evtInvitee in db.EventInviteeRelation on usr.Id equals evtInvitee.InviteeId 
+                              where (evtInvitee.EventId == e.Id) 
+                              select usr;
+            return users.ToArray<User>();
         }
     }
 }
---
+++ trunk/Meeting Organizer/InvitationDetails.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Meeting_Organizer
+{
+    public partial class InvitationDetails : Form
+    {
+        private Event evt = null;
+        private Database db = null;
+        public InvitationDetails()
+        {
+            InitializeComponent();
+        }
+        public InvitationDetails(Event e, Database db_):
+            this()
+        {
+            evt = e;
+            db = db_;
+            fromTextBox.Text = db.getUserWithId(e.CreatorId).Name;
+            titleTextBox.Text = e.Title;
+            descriptionTextBox.Text = e.Subject;
+            string invitees = null;
+            foreach (User user in db.getInviteesForEvent(e)) {
+                invitees += user.Name + "; ";
+            }
+            attendeesTextBox.Text = invitees;
+            duration.Text = e.Duration.ToString();
+            dateTime.Text = e.Start.ToString();
+        }
+
+        private void acceptButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void declineButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
