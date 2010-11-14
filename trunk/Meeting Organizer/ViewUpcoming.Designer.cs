4fa36cff213d907b4ed488e175af1f2b36c51b49
xiaolong.ch <xiaolong.ch@6ad2763e-12d5-d107-9526-56a298e86ecc>
2010-11-14 22:39:19 +0000

    made "View Upcoming" interface better looking

--- trunk/Meeting Organizer/ViewUpcoming.Designer.cs
+++ trunk/Meeting Organizer/ViewUpcoming.Designer.cs
@@ -34,61 +34,67 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.OK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(22, 13);
+            this.button1.BackColor = System.Drawing.Color.AliceBlue;
+            this.button1.Location = new System.Drawing.Point(12, 63);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(617, 46);
+            this.button1.Size = new System.Drawing.Size(682, 46);
             this.button1.TabIndex = 16;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(22, 65);
+            this.button2.BackColor = System.Drawing.Color.AliceBlue;
+            this.button2.Location = new System.Drawing.Point(12, 115);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(617, 46);
+            this.button2.Size = new System.Drawing.Size(682, 46);
             this.button2.TabIndex = 17;
             this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(22, 117);
+            this.button3.BackColor = System.Drawing.Color.AliceBlue;
+            this.button3.Location = new System.Drawing.Point(12, 167);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(617, 46);
+            this.button3.Size = new System.Drawing.Size(682, 46);
             this.button3.TabIndex = 18;
             this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(22, 169);
+            this.button4.BackColor = System.Drawing.Color.AliceBlue;
+            this.button4.Location = new System.Drawing.Point(12, 219);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(617, 46);
+            this.button4.Size = new System.Drawing.Size(682, 46);
             this.button4.TabIndex = 19;
             this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(22, 221);
+            this.button5.BackColor = System.Drawing.Color.AliceBlue;
+            this.button5.Location = new System.Drawing.Point(12, 271);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(617, 46);
+            this.button5.Size = new System.Drawing.Size(682, 46);
             this.button5.TabIndex = 20;
             this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // OK
             // 
-            this.OK.Location = new System.Drawing.Point(277, 313);
+            this.OK.Location = new System.Drawing.Point(315, 338);
             this.OK.Name = "OK";
             this.OK.Size = new System.Drawing.Size(75, 23);
             this.OK.TabIndex = 21;
@@ -96,11 +102,22 @@
             this.OK.UseVisualStyleBackColor = true;
             this.OK.Click += new System.EventHandler(this.OK_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(233, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 20);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Latest 5 Upcoming Events";
+            // 
             // ViewUpcoming
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(729, 373);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.OK);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -111,6 +128,7 @@
             this.Text = "ViewUpcoming";
             this.Load += new System.EventHandler(this.ViewUpcoming_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -122,5 +140,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button OK;
+        private System.Windows.Forms.Label label1;
 	}
 }
