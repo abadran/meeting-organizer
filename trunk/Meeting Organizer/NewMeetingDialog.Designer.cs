b0510296563dafc4bf14732b4adfa945cb5e0311
ahmed.badran@gmail.com <ahmed.badran@gmail.com@6ad2763e-12d5-d107-9526-56a298e86ecc>
2010-11-14 12:23:32 +0000

    Add Kai's new dialog this completes the sprint..latest and greatest code.

---
+++ trunk/Meeting Organizer/NewMeetingDialog.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Meeting_Organizer
+{
+    partial class NewMeetingDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.scheduleMeetingButton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.durationBox = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.durationLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.meetingHeldBeforeLabel = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.addAttendeeButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.durationBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelButton.Location = new System.Drawing.Point(164, 514);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(123, 23);
+            this.cancelButton.TabIndex = 17;
+            this.cancelButton.Text = "&Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // scheduleMeetingButton
+            // 
+            this.scheduleMeetingButton.Location = new System.Drawing.Point(233, 16);
+            this.scheduleMeetingButton.Name = "scheduleMeetingButton";
+            this.scheduleMeetingButton.Size = new System.Drawing.Size(123, 23);
+            this.scheduleMeetingButton.TabIndex = 16;
+            this.scheduleMeetingButton.Text = "&Display Available Time Slots";
+            this.scheduleMeetingButton.UseVisualStyleBackColor = true;
+            this.scheduleMeetingButton.Click += new System.EventHandler(this.scheduleMeetingButton_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox3.Controls.Add(this.richTextBox1);
+            this.groupBox3.Location = new System.Drawing.Point(0, 279);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(736, 229);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Description";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(730, 204);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(736, 10);
+            this.panel1.TabIndex = 18;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(736, 61);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Meeting Title";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 16);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(730, 42);
+            this.textBox1.TabIndex = 0;
+            // 
+            // durationBox
+            // 
+            this.durationBox.Controls.Add(this.textBox3);
+            this.durationBox.Controls.Add(this.label3);
+            this.durationBox.Controls.Add(this.numericUpDown1);
+            this.durationBox.Controls.Add(this.durationLabel);
+            this.durationBox.Controls.Add(this.label1);
+            this.durationBox.Controls.Add(this.meetingHeldBeforeLabel);
+            this.durationBox.Controls.Add(this.dateTimePicker1);
+            this.durationBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.durationBox.Location = new System.Drawing.Point(0, 71);
+            this.durationBox.Name = "durationBox";
+            this.durationBox.Size = new System.Drawing.Size(736, 70);
+            this.durationBox.TabIndex = 20;
+            this.durationBox.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Location = new System.Drawing.Point(140, 39);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(200, 20);
+            this.textBox3.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(573, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "hours";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(530, 12);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(37, 20);
+            this.numericUpDown1.TabIndex = 16;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // durationLabel
+            // 
+            this.durationLabel.AutoSize = true;
+            this.durationLabel.Location = new System.Drawing.Point(462, 16);
+            this.durationLabel.Name = "durationLabel";
+            this.durationLabel.Size = new System.Drawing.Size(47, 13);
+            this.durationLabel.TabIndex = 15;
+            this.durationLabel.Text = "Duration";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "The common time slot";
+            // 
+            // meetingHeldBeforeLabel
+            // 
+            this.meetingHeldBeforeLabel.AutoSize = true;
+            this.meetingHeldBeforeLabel.Location = new System.Drawing.Point(6, 16);
+            this.meetingHeldBeforeLabel.Name = "meetingHeldBeforeLabel";
+            this.meetingHeldBeforeLabel.Size = new System.Drawing.Size(110, 13);
+            this.meetingHeldBeforeLabel.TabIndex = 14;
+            this.meetingHeldBeforeLabel.Text = "Meeting to be held on";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(140, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.addAttendeeButton);
+            this.groupBox2.Controls.Add(this.scheduleMeetingButton);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 141);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(736, 119);
+            this.groupBox2.TabIndex = 21;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Attendees";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(3, 45);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(730, 67);
+            this.textBox2.TabIndex = 2;
+            // 
+            // addAttendeeButton
+            // 
+            this.addAttendeeButton.Location = new System.Drawing.Point(80, 16);
+            this.addAttendeeButton.Name = "addAttendeeButton";
+            this.addAttendeeButton.Size = new System.Drawing.Size(123, 23);
+            this.addAttendeeButton.TabIndex = 1;
+            this.addAttendeeButton.Text = "Add Attendee";
+            this.addAttendeeButton.UseVisualStyleBackColor = true;
+            this.addAttendeeButton.Click += new System.EventHandler(this.addAttendeeButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(450, 514);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "&Schedule Meeting";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // NewMeetingDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 539);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.durationBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.groupBox3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "NewMeetingDialog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New Meeting Details...";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.durationBox.ResumeLayout(false);
+            this.durationBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button scheduleMeetingButton;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox durationBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label durationLabel;
+        private System.Windows.Forms.Label meetingHeldBeforeLabel;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button addAttendeeButton;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
---
+++ trunk/Meeting Organizer/NewMeetingDialog.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Meeting_Organizer
+{
+    public partial class NewMeetingDialog : Form
+    {
+        public NewMeetingDialog()
+        {
+            InitializeComponent();
+        }
+
+        public NewMeetingDialog(Database db):
+            this()
+        {
+            this.db = db;
+        }
+
+        public string meetingTitle;
+
+        public string meetingDescription;
+
+        public User[] users;
+
+        public User u;
+
+        public int duration;
+
+        public DateTime date;
+
+        private Database db = null;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addAttendeeButton_Click(object sender, EventArgs e)
+        {
+            AddAttendeeDialog dlg = new AddAttendeeDialog();
+            dlg.u = u;
+            DialogResult result = System.Windows.Forms.DialogResult.Cancel;
+            result = dlg.ShowDialog();
+
+            if (result != System.Windows.Forms.DialogResult.Cancel)
+            {
+                users = dlg.users;
+                string user = "";
+                for(int i=0;i<users.Length;i++)
+                {
+                    user = user + users[i].Login + ";";
+                 }
+                textBox2.Text = user;
+            }
+        }
+
+        private void scheduleMeetingButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = System.Windows.Forms.DialogResult.Cancel;
+            
+            meetingTitle = textBox1.Text;
+            duration = int.Parse(numericUpDown1.Value.ToString());
+            meetingDescription = richTextBox1.Text;
+            date = dateTimePicker1.Value;
+
+            FreeTimeSlotDialog fts = new FreeTimeSlotDialog();
+
+            fts.meetingDescription = meetingDescription;
+            fts.meetingTitle = meetingTitle;
+            fts.date = date;
+            fts.duration = duration;
+            fts.users = users;
+
+            result = fts.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                int current = 8 + fts.selected;
+                date = date.AddHours(((double)0) - date.TimeOfDay.TotalHours + (double)current);
+                string timeslot = current.ToString() + ":00 - " + (current + duration).ToString() + ":00";
+                textBox3.Text = timeslot;
+            }
+        }
+
+        private bool computeTimeSlot()
+        {
+            bool res;
+            res = false;
+
+            return res;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            meetingDescription = richTextBox1.Text;
+
+            Event evt = new Event();
+            evt.CreatorId = u.Id;
+            evt.Title = meetingTitle;
+            evt.Subject = meetingDescription;
+            evt.Start = date;
+            evt.Duration = duration;
+
+            Invitation inv = new Invitation(u, db, evt, users);
+            inv.writeToDB();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
