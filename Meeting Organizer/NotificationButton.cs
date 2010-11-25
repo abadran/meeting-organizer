bd29cffc3df0e04719e3877a51c9975e939d2e53
ahmed.badran@gmail.com <ahmed.badran@gmail.com@6ad2763e-12d5-d107-9526-56a298e86ecc>
2010-11-25 09:04:09 +0000

    Renamed NotificationButton to InvitationNotificationButton

--- Meeting Organizer/NotificationButton.cs
+++ Meeting Organizer/NotificationButton.cs
@@ -5,13 +5,13 @@ using System.Text;
 
 namespace Meeting_Organizer
 {
-    public class NotificationButton: System.Windows.Forms.Button
+    public class InvitationNotificationButton: System.Windows.Forms.Button
     {
         //public Event evt { get; set; }
         //private Database db = null;
         public Invitation invitation { get; set; }
         //public NotificationButton(Event e, Database db_)
-        public NotificationButton(Invitation inv)
+        public InvitationNotificationButton(Invitation inv)
         {
             invitation = inv;
         }
